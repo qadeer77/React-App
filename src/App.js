0ab6aa19a5b9b80eb126a2c4bc09f8b342fcab18
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-11-07 21:33:20 +0500

    componenet-hello-world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = "hello world"
+const obj = {
+  name: "hello world",
+  fatehr: "data",
+}
 
 function App() {
   return (
     <div>
-      
+      <h1>{name}</h1>
+  
     </div>
   );
 }
